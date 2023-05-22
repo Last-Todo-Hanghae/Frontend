8016640f030617eee306d3d27b3557de688f6efb
Danee12 <9356471@gmail.com>
2023-05-22 20:31:17 +0900

    login.js 수정

--- src/Axios/login.js
+++ src/Axios/login.js
@@ -1,11 +1,9 @@
 import instance from "./api";
 
 const postLogIn = async (userInfo) => {
-    const response = await instance.post("/api/login", userInfo)
-    .then(res => {
-      console.log(res)
-      localStorage.setItem("token", res.accessToken)});
+    const response = await instance.post("/api/login", userInfo);
     return response;
   };
   
   export { postLogIn };
+  
